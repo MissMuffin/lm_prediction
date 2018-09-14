6a6d8890df1e5b7c92d12f51a90c75b0ac22521d
Miss Muffin <fyyree@gmail.com>
2018-09-14 14:42:01 +0200

    refactored input.py

---
+++ dump_emb.py
@@ -0,0 +1,140 @@
+import os
+import sys
+
+import numpy as np
+import tensorflow as tf
+from google.protobuf import text_format
+
+from config import Config
+from language_model.lm_1b import data_utils
+from load_model import LoadModel
+
+
+def remove_non_ascii_tokens(vocab, vocab_length=-1):
+    filtered_vocab = []
+    filtered_vocab_ids = []
+
+    if vocab_length != -1:
+        vsize = vocab_length
+    else:
+        vsize = vocab.size
+
+    for i in range(vsize):
+        token = vocab.id_to_word(i)
+        try:
+            token.encode('ascii')
+            filtered_vocab_ids.append(i)
+            filtered_vocab.append(token)
+        except UnicodeEncodeError:
+            continue
+    return filtered_vocab_ids, filtered_vocab
+
+
+def build_filtered_vocab(vocab, vocab_length=-1):
+
+    # set vocab output filename
+    if vocab_length != -1:
+        filename = Config.filename_vocab_short
+    else:
+        filename = Config.filename_vocab
+
+    # remove non ASCII chars from vocab
+    print("Building filtered vocab...")
+    filtered_vocab_ids, filtered_vocab = remove_non_ascii_tokens(vocab, vocab_length)
+
+    # write vocab to file
+    print("Writing filtered vocab in {}...".format(filename))
+    with open(filename, "w") as f:
+        f.write("\n".join(filtered_vocab))
+    print("- done. Wrote {} tokens".format(len(filtered_vocab)))
+
+    return filtered_vocab_ids
+
+
+def dump_lm(vocab, vocab_length=-1, dump_as_txt=False, dump_softmax=False, print_emb_status_every=100):
+
+    '''
+    Save softmax, fitlered vocab and embeddings to file 
+
+    Args:
+      vocab: Contains vocabulary size and converts word to ids
+      vocab_length: shorten the filtered vocab (also shortens embs)
+      dump_as_txt: save language model embeddings also as txt file
+      dum_softmax: save softmax to npy file
+      print_emb_status_every: prints a status msg every n token during embeddings loading
+    '''
+
+    inputs = np.zeros([Config.BATCH_SIZE, Config.NUM_TIMESTEPS], np.int32)
+    targets = np.zeros([Config.BATCH_SIZE, Config.NUM_TIMESTEPS], np.int32)
+    weights = np.ones([Config.BATCH_SIZE, Config.NUM_TIMESTEPS], np.float32)
+
+    sess, t = LoadModel(Config.pbtxt, Config.ckpt)
+
+    dim=1024
+
+    # dump softmax to file
+    if dump_softmax:
+        dump_softmax(sess, t, weights)
+
+    # create filtered vocab
+    vocab_ids = build_filtered_vocab(vocab, vocab_length)
+
+    # shorten vocab
+    if vocab_length != -1:
+        vocab_ids = vocab_ids[:vocab_length]
+
+    # set output files
+    if vocab_length != -1:
+        filename_emb_npy = Config.filename_emb_short.format(dim)
+        filename_emb_text = Config.filename_emb_text_short.format(dim)
+    else:
+        filename_emb_npy = Config.filename_emb.format(dim)
+        filename_emb_text = Config.filename_emb_text.format(dim)
+
+    # init embeddings tensor
+    all_embs = np.zeros([len(vocab_ids), 1024])
+
+    # collect embeddings tensor for each token in vocab
+    print("Starting to collect ", len(vocab_ids), " word embeddings...")
+    for i, word_id in enumerate(vocab_ids):
+        input_dict = {t['inputs_in']: inputs,
+                      t['targets_in']: targets,
+                      t['target_weights_in']: weights}
+        if 'char_inputs_in' in t:
+            input_dict[t['char_inputs_in']] = (
+                vocab.word_char_ids[word_id].reshape([-1, 1, Config.MAX_WORD_LEN]))
+
+        embs = sess.run(t['all_embs'], input_dict)
+        all_embs[i, :] = embs
+
+        if print_emb_status_every != -1:
+            if (i+1) % print_emb_status_every == 0:
+                print('Finished word embedding %d/%d - index[%d] %s' % (
+                    i+1, len(vocab_ids), i, vocab.id_to_word(word_id)))
+
+    print("Finished all", len(vocab_ids), "word embeddings")
+
+    # write embeddings to npy file
+    with tf.gfile.Open(filename_emb_npy, mode='w') as f:
+        np.save(f, all_embs)
+    print('Embeddings saved to npy file.')
+
+    # write embeddings to txt file
+    if dump_as_txt:
+        with tf.gfile.Open(filename_emb_text, mode='w') as f:
+            np.savetxt(f, all_embs)
+        print('Embeddings saved to txt file.')
+
+
+def dump_softmax(sess, t, weights):
+    softmax_weights = sess.run(t['softmax_weights'])
+    fname = Config.save_dir + '/embeddings_softmax.npy'
+    with tf.gfile.Open(fname, mode='w') as f:
+        np.save(f, softmax_weights)
+    print('Finished writing softmax weights to file.')
+
+
+# Vocabulary containing character-level information.
+vocab = data_utils.CharsVocabulary(Config.vocab_file, Config.MAX_WORD_LEN)
+
+dump_lm(vocab, 10, dump_as_txt=True, dump_softmax=False)
---
+++ lm_sampling.py
@@ -0,0 +1,68 @@
+import numpy as np
+
+from config import Config
+from language_model.lm_1b import data_utils
+from load_model import LoadModel
+
+
+def _SampleSoftmax(softmax):
+  return min(np.sum(np.cumsum(softmax) < np.random.rand()), len(softmax) - 1)
+
+def _SampleModel(prefix_words, vocab):
+  """Predict next words using the given prefix words.
+
+  Args:
+    prefix_words: Prefix words.
+    vocab: Vocabulary. Contains max word chard id length and converts between
+        words and ids.
+  """
+  targets = np.zeros([Config.BATCH_SIZE, Config.NUM_TIMESTEPS], np.int32)
+  weights = np.ones([Config.BATCH_SIZE, Config.NUM_TIMESTEPS], np.float32)
+
+  sess, t = LoadModel(Config.pbtxt, Config.ckpt)
+
+  if prefix_words.find('<S>') != 0:
+    prefix_words = '<S> ' + prefix_words
+
+  prefix = [vocab.word_to_id(w) for w in prefix_words.split()]
+  prefix_char_ids = [vocab.word_to_char_ids(w) for w in prefix_words.split()]
+
+  for _ in range(Config.NUM_SAMPLES):
+    inputs = np.zeros([Config.BATCH_SIZE, Config.NUM_TIMESTEPS], np.int32)
+    char_ids_inputs = np.zeros([Config.BATCH_SIZE, Config.NUM_TIMESTEPS, vocab.max_word_length], np.int32)
+    samples = prefix[:]
+    char_ids_samples = prefix_char_ids[:]
+    sent = ''
+    while True:
+        inputs[0, 0] = samples[0]
+        char_ids_inputs[0, 0, :] = char_ids_samples[0]
+        samples = samples[1:]
+        char_ids_samples = char_ids_samples[1:]
+
+        softmax = sess.run(t['softmax_out'],
+                            feed_dict={t['char_inputs_in']: char_ids_inputs,
+                                        t['inputs_in']: inputs,
+                                        t['targets_in']: targets,
+                                        t['target_weights_in']: weights})
+
+        sample = _SampleSoftmax(softmax[0])
+        sample_char_ids = vocab.word_to_char_ids(vocab.id_to_word(sample))
+
+        if not samples:
+            samples = [sample]
+            char_ids_samples = [sample_char_ids]
+        sent += vocab.id_to_word(samples[0]) + ' '
+        print('%s\n' % sent)
+
+        if (vocab.id_to_word(samples[0]) == '</S>' or
+            len(sent) > Config.MAX_SAMPLE_WORDS):
+            break 
+
+# Vocabulary containing character-level information.
+vocab = data_utils.CharsVocabulary(Config.vocab_file, Config.MAX_WORD_LEN)
+
+_SampleModel("With", vocab)
+# _SampleModel("Check", vocab)
+# _SampleModel("About", vocab)
+# _SampleModel("We", vocab)
+# _SampleModel("It", vocab)
